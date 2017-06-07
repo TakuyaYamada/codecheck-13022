83141b5eb707f3b8e017ab8e1e657d2c32181911
Takuya Yamada <yamada.takuya@ais.cmc.osaka-u.ac.jp>
2017-06-07 11:02:29 +0900

    websocket client

--- app/main.py
+++ app/main.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+import re
+from websocket import create_connection
+
+
 def main(argv):
-  for v in argv:
-    print(v)
+    pattern = r"Hello, I'm "
+
+    ws = create_connection(
+            "ws://challenge-server.code-check.io/api/websocket/hello")
+    result = ws.recv()
+
+    name = re.sub(pattern, '', result)
+    ws.send("Hello, {0}".format(name))
+    result = ws.recv()
+    print("{0} said: {1}".format(name, result))
